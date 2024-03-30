fe46b25ed08abddf10883dfc60804cffb10a0001
Davi <davi.ferreira100@gmail.com>
2024-03-30 20:49:55 -0300

    generate dataset with train, test, validation and label encoding file

--- src/pre_process.py
+++ src/pre_process.py
@@ -1,16 +1,16 @@
 import os
 import cv2
-import pickle
 from imutils import paths
 import shutil
 import argparse
 from face_detector import Face_detector
 
+
 def parse_args():
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('--path_images_raw', type=str, default="src/dataset_raw/Kashyap")
-    parser.add_argument('--path_images_processed', type=str, default="src/dataset_processed/")    
+    parser.add_argument('--path_images_raw', type=str, default="src/images_raw/")
+    parser.add_argument('--path_images_processed', type=str, default="src/images_processed/")    
 
     return parser.parse_args()
 
@@ -28,7 +28,6 @@ def save_image(cropped_face, path_to_save):
     detector = Face_detector()
     detector.save_cropped_face(cropped_face, path_to_save)
 
-    return
 
 def create_subdir(imagePaths, path_to_images, path_to_save):
     for sub_dir in imagePaths:
@@ -38,7 +37,7 @@ def create_subdir(imagePaths, path_to_images, path_to_save):
 
 def main():
     args = parse_args()
-
+    
     path_to_images = args.path_images_raw
     path_to_save_processed_images  = args.path_images_processed
 
@@ -46,10 +45,10 @@ def main():
         shutil.rmtree(path_to_save_processed_images)
         os.mkdir(path_to_save_processed_images)
     
-    imagePaths = list(paths.list_images(path_to_images))
-    create_subdir(imagePaths, path_to_images, path_to_save_processed_images)
-    
-    for sub_dir in imagePaths:
+    image_paths = list(paths.list_images(path_to_images))
+    create_subdir(image_paths, path_to_images, path_to_save_processed_images)
+
+    for sub_dir in image_paths:
         path_to_save = sub_dir.replace(path_to_images, path_to_save_processed_images)
         cropped_face = process_image(sub_dir)
         if cropped_face is not None:
