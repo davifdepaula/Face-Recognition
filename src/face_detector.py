7d8dff83b269d1c2810ea5aee58b445fd67ca83d
Davi <davi.ferreira100@gmail.com>
2024-03-30 03:36:37 -0300

    refactor: modified logic to get face position

--- src/face_detector.py
+++ src/face_detector.py
@@ -1,30 +1,44 @@
 import cv2
+import numpy as np
 
 class Face_detector:
 
     def __grayscale_image(self, image):
         return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
+    
+    def __distance(self, pointA, pointB):
+        dist = np.linalg.norm(pointA - pointB)
+        return dist
+    
+    def __get_min_distance(self, array):
+        min = float("inf") 
+        min_position = 0
+        for i, position in enumerate(array):
+            if self.__distance(position[1], position[2]) < min:
+                min = self.__distance(position[1], position[2])
+                min_position = position[:]
+        return min_position
+        
     def get_face_postion(self, image):
         gray_image = self.__grayscale_image(image)
         face_classifier = cv2.CascadeClassifier(
-        cv2.data.haarcascades + "haarcascade_frontalface_default.xml"
-)
-        face_positions = face_classifier.detectMultiScale(gray_image, 1.3, 5)[0]
+        cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
 
-        return face_positions
-    
-    def crop_face(self, image, face_positions):
-        x, y, h, w = face_positions
-        cropped_image = image[y:y+h, x:x+w]
+        face_positions = face_classifier.detectMultiScale(gray_image, 1.2, 5)
+        if len(face_positions) == 0:
+            return None
         
+        if len(face_positions) == 1:
+            return face_positions[0]
+        
+        if len(face_positions) > 1:
+            return self.__get_min_distance(face_positions)
+            
+    def crop_face(self, image, face_positions):
+        x, y, w, h = face_positions
+        cropped_image = image[y:y+h, x:x+w]
+
         return cropped_image
     
-    def save_cropped_face(self, image, file_name, path_to_save = None):
-        if path_to_save:
-            cv2.imwrite(file_name, image, path_to_save)
-            return
-        cv2.imwrite(file_name, image)
-
-
-    
+    def save_cropped_face(self, image, path_to_save):
+        cv2.imwrite(path_to_save, image)
